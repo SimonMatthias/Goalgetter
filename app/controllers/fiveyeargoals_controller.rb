96affa3394ebd6923925086a53346ba618cc13c9
anna schäffner <anna-schaeffner@gmx.de>
2023-03-02 20:11:27 +0100

    added fiveyeargoalcontroller, dashboard route, dashboard erb

--- app/controllers/fiveyeargoals_controller.rb
+++ app/controllers/fiveyeargoals_controller.rb
@@ -1,2 +1,50 @@
 class FiveyeargoalsController < ApplicationController
+  before_action :set_fiveyeargoal, only: [:show, :edit, :update, :destroy]
+  # skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @fiveyeargoals = Fiveyeargoal.all
+  end
+
+  def show
+    @fiveyeargoal = Fiveyeargoal.find(params[:id])
+  end
+
+  def new
+    @fiveyeargoal = Fiveyeargoal.new
+  end
+
+  def create
+    @user = current_user
+    @fiveyeargoal = Fiveyeargoal.new(fiveyeargoal_params)
+    @fiveyeargoal.user = @user
+    if @fiveyeargoal.save
+      # redirect_to yearlygoal_path(@ygoal) unclear about route
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @fiveyeargoal.update(fiveyeargoal_params)
+    redirect_to dashboard_path
+  end
+
+  def destroy
+    @fiveyeargoal.destroy
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def set_fiveyeargoal
+    @fiveyeargoal = Fiveyeargoal.find(params[:id])
+  end
+
+  def fiveyeargoal_params
+    params.require(:fiveyeargoal).permit(:title, :description, :category, :done)
+  end
 end
