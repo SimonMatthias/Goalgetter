3249ca4ab39451a949ef284a29f78cfd4ef7ffe6
Samuel Schneider <samuel@skschneider.ch>
2023-03-04 16:17:54 +0100

    forms

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get '/dashboard', to: 'pages#dashboard', as: :dashboard
+  get '/fiveyeargoals', to: 'fiveyeargoals#index', as: :fiveyeargoals
+  get '/yearlygoals', to: 'yearlygoals#index', as: :yearlygoals
+  get '/quarterlygoals', to: 'quarterlygoals#index', as: :quarterlygoals
 end
