e5e857077e02d56dc3bd972b2977aa1709996a69
anna schäffner <anna-schaeffner@gmx.de>
2023-03-18 16:54:49 +0100

    bar and numbers working

--- app/javascript/controllers/progressbar_controller.js
+++ app/javascript/controllers/progressbar_controller.js
@@ -6,19 +6,30 @@ export default class extends Controller {
   static classes = ['progress-small']
 
   connect() {
-    console.log(this.numericTarget.innerHTML)
   }
 
+
+
   changeHeight () {
-    console.log("hello again?")
+    // console.log(this.numericTarget.innerHTML)
+    var numeric = parseFloat(this.numericTarget.innerHTML)
+    // console.log(numeric)
+
     if (this.checkTarget.checked == true && this.btnValueTarget.innerHTML == this.quarterlyTarget.innerHTML) {
-    this.barTarget.style.height = "40%"
-    // this.numericTarget.innerHTML += (1);
+    this.barTarget.style.height =  parseFloat(this.barTarget.dataset.height) + 10 + '%'
+    this.barTarget.dataset.height =  parseFloat(this.barTarget.dataset.height) + 10
+    // numeric +*;
     }
     else if (this.checkTarget.checked == false && this.btnValueTarget.innerHTML == this.quarterlyTarget.innerHTML) {
-    this.barTarget.style.height = "10%";
-    // this.numericTarget.innerHTML -= (1);
+    this.barTarget.style.height =  parseFloat(this.barTarget.dataset.height) - 10 + '%'
+    this.barTarget.dataset.height =  parseFloat(this.barTarget.dataset.height) - 10
+    // console.log("else")
+    numeric --;
+    // console.log (numeric, "ey")
+    this.numericTarget.textContent = numeric
+
     }
 
-}
+  }
+
 }
