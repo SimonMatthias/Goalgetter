dbbbd74090e40ae2d210a6375c578d06cfa1b023
anna schäffner <anna-schaeffner@gmx.de>
2023-03-18 16:56:22 +0100

    bar and numbers dashboard working

--- app/javascript/controllers/progressbar_controller.js
+++ app/javascript/controllers/progressbar_controller.js
@@ -2,8 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="progressbar"
 export default class extends Controller {
-  static targets =['check', 'bar', 'btnValue', 'quarterly', 'numeric']
-  static classes = ['progress-small']
+  static targets =['check', 'bar', 'btnValue', 'quarterly', 'numeric', 'topNumber']
 
   connect() {
   }
@@ -11,23 +10,27 @@ export default class extends Controller {
 
 
   changeHeight () {
-    // console.log(this.numericTarget.innerHTML)
     var numeric = parseFloat(this.numericTarget.innerHTML)
-    // console.log(numeric)
+    var numericTotal= parseFloat(this.topNumberTarget.textContent)
+    // console.log(numericTotal, "aha")
 
     if (this.checkTarget.checked == true && this.btnValueTarget.innerHTML == this.quarterlyTarget.innerHTML) {
-    this.barTarget.style.height =  parseFloat(this.barTarget.dataset.height) + 10 + '%'
-    this.barTarget.dataset.height =  parseFloat(this.barTarget.dataset.height) + 10
-    // numeric +*;
+      this.barTarget.style.height =  parseFloat(this.barTarget.dataset.height) + 10 + '%'
+      this.barTarget.dataset.height =  parseFloat(this.barTarget.dataset.height) + 10
+      numeric ++;
+      this.numericTarget.textContent = numeric
+      numericTotal ++;
+      this.topNumberTarget.textContent = numericTotal
     }
     else if (this.checkTarget.checked == false && this.btnValueTarget.innerHTML == this.quarterlyTarget.innerHTML) {
-    this.barTarget.style.height =  parseFloat(this.barTarget.dataset.height) - 10 + '%'
-    this.barTarget.dataset.height =  parseFloat(this.barTarget.dataset.height) - 10
-    // console.log("else")
-    numeric --;
-    // console.log (numeric, "ey")
-    this.numericTarget.textContent = numeric
+      this.barTarget.style.height =  parseFloat(this.barTarget.dataset.height) - 10 + '%'
+      this.barTarget.dataset.height =  parseFloat(this.barTarget.dataset.height) - 10
+      // console.log("else")
+      numeric --;
+      this.numericTarget.textContent = numeric
 
+      numericTotal --
+      this.topNumberTarget.textContent = numericTotal
     }
 
   }
