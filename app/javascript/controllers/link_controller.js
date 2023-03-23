3f4e06a51767d764511141fd4553aa9dd9ca8fa3
anna schäffner <anna-schaeffner@gmx.de>
2023-03-23 19:24:14 +0100

    working toggle on links in navbar

--- app/javascript/controllers/link_controller.js
+++ app/javascript/controllers/link_controller.js
@@ -5,35 +5,12 @@ export default class extends Controller {
   static targets = ["linkTo"]
 
   connect() {
-    // console.log(window.location.pathname)
-    // console.log(this.linkToTargets)
-    // console.log(window.location.pathname)
-  };
-
-  changeLink (){
     const linkToArray = this.linkToTargets
 
     linkToArray.forEach(linkTo => {
       if (window.location.pathname !== linkTo.pathname) {
         linkTo.classList.remove("hidden")
-        // window.location.reload()
-
-        console.log("hoi")
-      // linkTo.classList.remove("hidden")
     }
     })
-    // linkToArray.forEach(linkTo => {
-
-    //   console.log(linkTo.pathname)
-    //   if (window.location.pathname == linkTo.pathname){
-    //     linkTo.classList.add("hidden")
-    //     console.log("hi")
-    //   }
-    //   else if (window.location.pathname !== linkTo.pathname) {
-    //     linkTo.classList.remove("hidden")
-    //     console.log("hoi")
-
-    //   }
-    // });
   };
-}
+};
